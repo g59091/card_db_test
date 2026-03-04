b8519c920956cbd2e2784d719a30045c4c29560a
Fulaaa <carlow00x@gmail.com>
2026-03-03 21:59:36 -0500

    refactored login to use firebase documents and collection

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,10 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
 from flask import Flask, render_template, request, redirect, session, url_for
 from dotenv import load_dotenv
 import mysql.connector
 from flask_cors import CORS, cross_origin
-import bcrypt, os, requests
+import bcrypt, os, requests, json
 
 # Flask load
 load_dotenv()
@@ -13,24 +15,24 @@ app.secret_key = os.getenv("SECRET_KEY")
 # CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 CORS(app)
+
 # app.config['CORS_HEADERS'] = "Content-Type"
 
 # MySQL DB config
-db_config = {
-  'host': os.getenv("DB_HOST"),
-  'user': os.getenv("DB_USER"),
-  'password': os.getenv("DB_PASSWORD"),
-  'database': os.getenv("DB_NAME")
-}
+# db_config = {
+#   'host': os.getenv("DB_HOST"),
+#   'user': os.getenv("DB_USER"),
+#   'password': os.getenv("DB_PASSWORD"),
+#   'database': os.getenv("DB_NAME")
+# }
 
-# Goal: Configure FireStore connection inside app.py 
-# import firebase_admin
-# from firebase_admin import credentials, firestore
 
-# cred = credentials.Certificate("firebase-service-account.json")
-# firebase_admin.initialize_app(cred)
+cred = credentials.Certificate(
+  json.loads(os.getenv("FIREBASE_SERVICE_ACCOUNT"))
+)
+firebase_admin.initialize_app(cred)
 
-# db = firestore.client()
+db = firestore.client()
 
 # auth routes
 @app.route("/register", methods=['POST', 'GET'])
@@ -39,18 +41,28 @@ def register():
     username = request.form["username"]
     email = request.form["email"]
     hashed_password = bcrypt.hashpw(request.form["password"].encode("utf-8"), bcrypt.gensalt())
+    print(username,email,hashed_password)
 
     # Goal: Migrate to using Users Colection
     # For each UserID create Inventory Collection /w Card Details
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-    cursor.execute("""
-        INSERT INTO user (username, email, pass_hash)
-        VALUES (%s, %s, %s)
-    """, (username, email, hashed_password))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    # Note : Unique random ID
+    sign_up_ref =  db.collection("Users").document(username).collection("Details").document("SignUpDetails")
+
+    sign_up_ref.set({
+      "Email": email,
+      "PassHash": hashed_password,
+      "Username": username
+    })
+
+    # conn = mysql.connector.connect(**db_config)
+    # cursor = conn.cursor()
+    # cursor.execute("""
+    #     INSERT INTO user (username, email, pass_hash)
+    #     VALUES (%s, %s, %s)
+    # """, (username, email, hashed_password))
+    # conn.commit()
+    # cursor.close()
+    # conn.close()
 
     return redirect(url_for("app.login"))
   return render_template("register.html")
@@ -64,19 +76,22 @@ def login():
     # Goal: Migrate to Using Users Collection
     # Check across each UserID find the Details Collection
     # Check Users info
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute("""
-      SELECT * FROM user WHERE username = %s
-    """, (username))
-    user = cursor.fetchone()
-    cursor.close()
-    conn.close()
+    # conn = mysql.connector.connect(**db_config)
+    # cursor = conn.cursor(dictionary=True)
+    # cursor.execute("""
+    #   SELECT * FROM user WHERE username = %s
+    # """, (username))
+    # user = cursor.fetchone()
+    # cursor.close()
+    # conn.close()
+    sign_up_ref =  db.collection("Users").document(username).collection("Details").document("SignUpDetails")
+    user = sign_up_ref.get()
 
     # return user on pass chk
-    if user and bcrypt.checkpw(request.form["password"].encode("utf-8"), user["pass_hash"].encode("utf-8")):
-      session["user_id"] = user["user_id"]
-      session["username"] = user["username"]
+    # Change : consolidating user_id with username
+    if user.exists and bcrypt.checkpw(request.form["password"].encode("utf-8"), user["PassHash"].encode("utf-8")):
+     # session["user_id"] = user["user_id"]
+      session["username"] = user["Username"]
       return redirect(url_for("app.search"))
     else:
       return "Please enter a valid username & password YB"
@@ -100,75 +115,77 @@ def search():
 
     # Goal:Try searching through Users Collection 
     # Check Inventory Collection if Card is present
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor(dictionary=True)
-    try:
+    # conn = mysql.connector.connect(**db_config)
+    # cursor = conn.cursor(dictionary=True)
+    # try:
+    # cursor.execute("""
+    #   SELECT * FROM card WHERE name LIKE %s
+    # """, ('%' + card_name + '%'))
+    # results = cursor.fetchall()
+    # result = results[0] if results else None
+    inv_ref =  db.collection("Users").document(session["username"]).collection("Inventory")
+    cards = inv_ref.where("CardName", "==", card_name).get()
+
+    # Adjust: Approach searching for Card inside of Inventory
+    if result:
+      card_id = result['card_id']
+      user_id = session.get("user_id", 1)
       cursor.execute("""
-        SELECT * FROM card WHERE name LIKE %s
-      """, ('%' + card_name + '%'))
-      results = cursor.fetchall()
-      result = results[0] if results else None
+        SELECT * FROM usercardinventory
+        WHERE user_id = %s AND card_id = %s
+      """, (user_id, card_id))
+      in_inv_flag = cursor.fetchone() is not None
+    else:
+      magic_url = f"https://api.scryfall.com/cards/named?fuzzy={card_name}"
+      response = requests.get(magic_url)
+      print(response)
+      # on successful response
+      if response.status_code == 200:
+        card_data = response.json()
+        set_name = card_data['set_name']
+        release_date = card_data['released_at']
 
-      # Adjust: Approach searching for Card inside of Inventory
-      if result:
-        card_id = result['card_id']
-        user_id = session.get("user_id", 1)
+        # Adjust: Consider removing these executes
         cursor.execute("""
-          SELECT * FROM usercardinventory
-          WHERE user_id = %s AND card_id = %s
-        """, (user_id, card_id))
-        in_inv_flag = cursor.fetchone() is not None
-      else:
-        magic_url = f"https://api.scryfall.com/cards/named?fuzzy={card_name}"
-        response = requests.get(magic_url)
-        print(response)
-        # on successful response
-        if response.status_code == 200:
-          card_data = response.json()
-          set_name = card_data['set_name']
-          release_date = card_data['released_at']
+          SELECT set_id FROM cardset WHERE name = %s
+        """, (set_name))
+        set_row = cursor.fetchone()
 
-          # Adjust: Consider removing these executes
-          cursor.execute("""
-            SELECT set_id FROM cardset WHERE name = %s
-          """, (set_name))
-          set_row = cursor.fetchone()
-
-          if set_row:
-            set_id = set_row['set_id']
-          else:
-            cursor.execute("""
-              INSERT INTO cardset (name, release_date) VALUES (%s, %s)
-            """, (set_name, release_date))
-            conn.commit()
-            set_id = cursor.lastrowid
-
-          # Goal: Refactor to add Card Details and Inventory
-          cursor.execute("""
-            INSERT INTO card (name, type, rarity, mana_cost, rules_text, image_url, set_id)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
-          """, (
-            card_data['name'],
-            card_data['type_line'],
-            card_data['rarity'].capitalize(),
-            card_data.get('mana_cost', ''),
-            card_data.get('oracle_text', ''),
-            card_data['image_uris']['normal'],
-            set_id
-          ))
-          conn.commit()
-
-          # Adjust: Consider removing this execute
-          cursor.execute("""
-            SELECT * FROM card WHERE name LIKE %s
-          """, (card_data['name']))
-          result = cursor.fetchone()
-          message = "magic_url - card found"
+        if set_row:
+          set_id = set_row['set_id']
         else:
-          message = "magic_url - card not found"
-    finally:
-      cursor.close()
-      conn.close()
+          cursor.execute("""
+            INSERT INTO cardset (name, release_date) VALUES (%s, %s)
+          """, (set_name, release_date))
+          conn.commit()
+          set_id = cursor.lastrowid
+
+        # Goal: Refactor to add Card Details and Inventory
+        cursor.execute("""
+          INSERT INTO card (name, type, rarity, mana_cost, rules_text, image_url, set_id)
+          VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """, (
+          card_data['name'],
+          card_data['type_line'],
+          card_data['rarity'].capitalize(),
+          card_data.get('mana_cost', ''),
+          card_data.get('oracle_text', ''),
+          card_data['image_uris']['normal'],
+          set_id
+        ))
+        conn.commit()
+
+        # Adjust: Consider removing this execute
+        cursor.execute("""
+          SELECT * FROM card WHERE name LIKE %s
+        """, (card_data['name']))
+        result = cursor.fetchone()
+        message = "magic_url - card found"
+      else:
+        message = "magic_url - card not found"
+    # finally:
+    #   cursor.close()
+    #   conn.close()
   # return render_template("search.html", result=result, message=message, in_inv_flag=in_inv_flag)
   return {"result" :result, "message":message, "in_inv_flag":in_inv_flag}
 
---
+++ start_backend.ps1
@@ -0,0 +1,5 @@
+# Activating the backend
+
+& .\backend\.venv\Scripts\Activate.ps1
+
+python backend\app\app.py
